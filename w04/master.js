5daede7842ae8019347cf55f6bf8369093426944
shiningsack <144655601+shiningsack@users.noreply.github.com>
2023-10-10 15:53:16 +0900

    Add files via upload

---
+++ w04/master.js
@@ -0,0 +1,43 @@
+function linksetColor(color){
+    var alist = document.querySelectorAll('a');
+      var i = 0;
+      while (i < alist.length) {
+        alist[i].style.color = color;
+        i = i + 1;
+      }
+  }
+  function bodySetColor(color){
+    document.querySelector('body').style.color=color;  
+  }
+  function bodyBgSetColor(color){
+    document.querySelector('body').style.backgroundColor = color;
+  }
+  
+function nightDayHandler(self){
+    var target = document.querySelector('body');      
+    if(self.value == 'day'){
+      target.style.backgroundColor = 'white';
+      target.style.color = 'black';
+      self.value = 'night';
+      document.querySelector('#night_day_2').value = 'night';
+      var alist = document.querySelectorAll('a');
+      var i = 0;
+      while (i < alist.length) {
+        alist[i].style.color = 'blue';
+        i = i + 1;
+      } 
+    } else {
+      target.style.backgroundColor = 'black';
+      target.style.color = 'white';
+      self.value = 'day';
+      document.querySelector('#night_day_2').value = 'day';
+      var alist = document.querySelectorAll('a');
+      var i = 0;
+      while (i < alist.length) {
+        alist[i].style.color = 'powderblue';
+        i = i + 1;
+      } 
+    }
+  }
+  
+  
